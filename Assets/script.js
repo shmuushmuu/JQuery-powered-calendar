81ac7753b2ce44594b32084c4dc4ba2758edd4ed
Sam Craig <samfcraig@gmail.com>
2022-12-07 21:06:30 -0500

    added  and now it all works. yay

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,27 @@
 $(function dynamicPage() {
-  var currentTime = dayjs().hour();
+
   var saveButton = $('.saveBtn');
+  var currentTime = dayjs().hour();
+  
+$('.description').each(function(){
+
   
   var hourBlock = parseInt($(this).parent().attr('id').replace('hour-', ''));
-    if (hourBlock < currentTime){
+  if (hourBlock < currentTime){
     $(this).parent().addClass('past')
   } else if(hourBlock === currentTime) {
     $(this).parent().addClass('present')
   } else {
     $(this).addClass('future');
   }
-
+  
   var currDate = $('#currentDay');
-        currDate.text(new Date());
+  currDate.text(new Date());
 });
 
 $(saveButton).click(function() {
   var textInput = $(this).siblings('.description').val();
   var time = $(this).parent().attr('id').split('-')[1];
   localStorage.setItem(textInput, time);
+})
 })
