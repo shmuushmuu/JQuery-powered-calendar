05fe49251a41fb9321a68ee4a9a10ddde9b038a8
Sam Craig <samfcraig@gmail.com>
2022-12-08 21:26:30 -0500

    added comments to script to explain everything. finished README.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # JQuery-powered-calendar
+[Here's the quiz!] (https://shmuushmuu.github.io/JQuery-powered-calculator/)
+
+
+## Table of Contents
+- [Description] (#description-of-project)
+- [Visuals] (#visuals)
+
+## Description of Project
+The project was to make a day-planner that would interact with real-time, save a to-do list to local storage, and repopulate the text areas upon refresh.
+Most of the html was already provided, and the CSS was not to be touched. I learned to use JQuery in a hands-on manner, and I'm in love. If I saw JQuery on J-Swipe, I'd function (swipeRight).
+
+I was able to wrap all of my JS in one function, which kept the page dynamic. It was enjoyable to accomplish my goal, while frustrating at times of struggle (like every project). I was able to find myself more assuredly searching for answers to questions in addition to asking questions of my fellow classmates. The more I learn about this field, the more I love the developer community.
+
+## Visuals
+The day-planner should look like this:
+
+![Screenshot] (assets/images/scheduler-screenshot.png)
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,27 @@
+// this function wraps the whole of the script, so the variables and functions all relate to each other
 $(function dynamicPage() {
 
+  // calling my variables that aren't explicitly named in my functions:
+  // the save button from the html, what time and day it is via dayjs,
+  // what the current date is for comparing later, and having the date 
+  // display in the header
   var saveButton = $('.saveBtn');
   var currentTime = dayjs().hour();
+  var currDate = $('#currentDay');
+  currDate.text(new Date());
   
+  // when a user types in the text area and click the save icon, the text and the hour's id are logged in local storage
   $(saveButton).click(function() {
     var textInput = $(this).siblings('.description').val();
     var time = $(this).parent().attr('id');
     localStorage.setItem(time, textInput);
   })
   
+  // rather than have mutiple lines of html, I locked onto the class .description,
+  // which was the only part I wanted to have changing background colors. This
+  // function compares the time of day to the hours in the hour block, and
+  // changes the background color to match (gray-past), (red-present), and (green-future).
+
   $('.description').each(function(){
     var hourBlock = parseInt($(this).parent().attr('id').replace('hour-', ''));
     if (hourBlock < currentTime){
@@ -19,10 +32,9 @@ $(function dynamicPage() {
     $(this).addClass('future');
   }
   
+  // this calls back the value saved into local storage and keeps it in the text area, even after
+  // the page is refreshed
   var text = localStorage.getItem($(this).parent().attr('id'));
   $(this).val(text);
 });
-
-  var currDate = $('#currentDay');
-  currDate.text(new Date());
 })
