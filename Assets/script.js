7bb01d84dc40743581ced4a7943859c0fd004024
Sam Craig <samfcraig@gmail.com>
2022-12-08 19:40:17 -0500

    tried other things. didn't work. had a breakdown. bon appetit.

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,9 @@ $(function dynamicPage() {
   var saveButton = $('.saveBtn');
   var currentTime = dayjs().hour();
   
-$('.description').each(function(){
-
-  var hourBlock = parseInt($(this).parent().attr('id').replace('hour-', ''));
-  if (hourBlock < currentTime){
+  $('.description').each(function(){
+    var hourBlock = parseInt($(this).parent().attr('id').replace('hour-', ''));
+    if (hourBlock < currentTime){
     $(this).parent().addClass('past')
   } else if(hourBlock === currentTime) {
     $(this).parent().addClass('present')
@@ -22,6 +21,6 @@ $(saveButton).click(function() {
   var textInput = $(this).siblings('.description').val();
   var time = $(this).parent().attr('id');
   localStorage.setItem(textInput, time);
-  localStorage.getItem(textInput);
-  })
+})
+  var returnedText = JSON.parse(localStorage.getItem(textInput));
 })
